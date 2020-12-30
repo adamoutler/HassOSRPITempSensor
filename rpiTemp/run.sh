8fa7d3c659748be043c9daaa9f92ae3fdb81af0a
root <root@a0d7b954-ssh.local.hass.io>
2020-12-30 18:18:58 -0500

    Adding readme and configurable C or F option

--- rpiTemp/run.sh
+++ rpiTemp/run.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/with-contenv bashio
+CorF=$(cat options.json |jq -r '.CorF')
 until false; do
-  cpuTempC=$(( $(cat /sys/class/thermal/thermal_zone0/temp ) / 1000  ))
-  cpuTempF=$(( ( $cpuTempC *  9/5 ) + 32 ))
-  echo "Current Temperature $cpuTempC"C" $cpuTempF"F
-  curl -s -X POST -H "Content-Type: application/json"  -H "Authorization: Bearer $HASSIO_TOKEN" -d '{"state": "'$cpuTempF'", "attributes":  {"unit_of_measurement": "F", "icon": "mdi:clock-start", "friendly_name": "CPU Temp"}}' http://hassio/homeassistant/api/states/sensor.cpu_temp
+  cpuTemp=$(( $(cat /sys/class/thermal/thermal_zone0/temp ) / 1000  ))
+  unit="C"
+  if [ $CorF == "F" ]; then
+    cpuTemp=$(( ( $cpuTemp *  9/5 ) + 32 ));
+    unit="F"
+  fi
+    
+  echo "Current Temperature $cpuTemp °$unit"
+  curl -s -X POST -H "Content-Type: application/json"  -H "Authorization: Bearer $HASSIO_TOKEN" -d '{"state": "'$cpuTemp'", "attributes":  {"unit_of_measurement": "°'$unit'", "icon": "mdi:clock-start", "friendly_name": "CPU Temp"}}' http://hassio/homeassistant/api/states/sensor.cpu_temp
   sleep 10;
 done
